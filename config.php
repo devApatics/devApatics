f9c7278c8147939c531b7b7c058947659e8915ce
kalpit_belani <kalpit.belani@plutustec.com>
2021-08-31 18:40:47 +0530

    Solved the capcha issue.

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+	// --------------------------------- Mail configuration --------------------------------//
+ 
+	//Client mail cred.
+	$mail_user_name = getenv("smtpEmail"); // SMTP username 
+	$mail_password = getenv("smtpPWD"); // SMTP password 
+
+	$to_admin_email = getenv("toEmail"); // Set this to email send to admin 
+
+	// -------------------------------Google captcha--------------------------------------// 
+ 
+	$google_secret_key = getenv("googleSecretKey");   
+	
+	// ----------------------------------- DB Configuration -----------------------------------// 
+	// DB Cred.
+	$servername = getenv("host"); 
+	$username = getenv("UId");
+	$password = getenv("dbPWD");
+	$database = getenv("database"); 
+
+	$conn = new mysqli($servername, $username, $password, $database); // For DB Connection
+
+	// Check DB connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+?>
--- site_key.php
+++ site_key.php
@@ -1,11 +1,7 @@
 <?php
    // -------------------------------Google captcha--------------------------------------// 
-	
-	// web-stage.apatics.io
-	$google_site_key = '6LczCRIcAAAAAGictKN182hHkpZGrGcy1LjDCP4m';
-	
-	// apatics.net
-	// $google_site_key = '6Ld0ChIcAAAAAH2qO-F4wDxfHL9UVgs_X9LaatbT';
-    
+	 
+	$google_site_key = getenv("googleSiteKey"); 
+
     return print_r($google_site_key);
 ?>
